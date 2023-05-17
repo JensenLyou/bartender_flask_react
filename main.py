a83897403dfbe21d5434d81e0a47049344ec4a17
seanpixel <112411911+seanpixel@users.noreply.github.com>
2023-05-17 09:44:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 #AGENT_NAME = os.getenv("AGENT_NAME", "my-agent")
-AGENT_NAME = "agent"
+AGENT_NAME = "Scrooge"
 
 agent = Agent(AGENT_NAME)
 
@@ -26,19 +26,15 @@ while True:
         if (userInput.startswith("read:")):
             agent.read(" ".join(userInput.split(" ")[1:]))
             print("Understood! The information is stored in my memory.")
-        elif (userInput.startswith("identity:")):
-            print("New system prompt:")
-            agent.set_init_prompt(" ".join(userInput.split(" ")[1:]))
-        elif (userInput.startswith("think:")):
-            agent.think(" ".join(userInput.split(" ")[1:]))
-            print("Understood! I stored that thought into my memory.")
-        elif (userInput.startswith("readDoc:")):
-            agent.readDoc(" ".join(userInput.split(" ")[1:]))
-            print("Understood! I stored the document into my memory.")
         elif (userInput.startswith("newUser:")):
             curUser = " ".join(userInput.split(" ")[1:])
             agent.setUser(curUser)
             print(f"New user set to {curUser}")
+        elif (userInput == "clear"):
+            agent.clearMemory()
+            print("Memory Cleared")
+        elif (userInput == "toggleThoughts"):
+            agent.seeThoughts = not agent.seeThoughts
         else:
             print(agent.action(userInput), "\n")
     else:
